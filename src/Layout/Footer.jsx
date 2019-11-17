8a97a1c58bd2d87d777dc4c78a6a20ae5e4435a8
KleinosFR <kleinos@hotmail.com>
2019-11-17 15:49:42 +0100

    added responsive to footer, and implemented react router routes

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink,} from 'reactstrap';
+import {useHistory} from "react-router-dom"
 
 import "./footer.css"
 
-const Footer = (props) => {
+function Footer (props) {
+
+let history=useHistory()
+
+const handleLink = (path) =>{
+
+    history.push(path)
+}
+
 
   return (
     <footer id="footer" className="">
-        <Navbar color="light" light expand="xs" className="">
-            <NavbarBrand>TRACER-O</NavbarBrand>
-            <Nav className="mr-auto" navbar>
+        <Navbar color="light" light expand="lg" className="">
+            <NavbarBrand className="col-12 col-lg-1">TRACER-O</NavbarBrand>
+            <Nav className="mr-auto col-12 col-lg-6 justify-content-center my-2" navbar>
                 <NavItem>
-                    <NavLink>Accueil</NavLink>
+                    <NavLink onClick={() => handleLink("/")}>Accueil</NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink>Contact</NavLink>
@@ -19,10 +28,14 @@ const Footer = (props) => {
                 <NavItem>
                     <NavLink>Devis en ligne</NavLink>
                 </NavItem>
-            </Nav>
-            <Nav className="ml-auto" navbar>
                 <NavItem>
-                    Made with ❤ by Stéphane LAVAUD  - All rights reserved 2019
+                    <NavLink>Mentions légales</NavLink>
+                </NavItem>
+
+            </Nav>
+            <Nav className="ml-auto col-lg-4 my-2" navbar>
+                <NavItem>
+                    Made with ❤ by <a className="text-dark" href="https://stephane-lavaud.dev">Stéphane LAVAUD</a>  - All rights reserved 2019
                 </NavItem>
             </Nav>
             
