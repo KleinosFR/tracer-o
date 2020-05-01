1d4beb69e8b3a61d85d628171e40440ea2d1bcc1
KleinosFR <kleinos@hotmail.com>
2020-05-01 16:28:33 +0200

    fixed broken destination image

--- src/HomePage/Destination.jsx
+++ src/HomePage/Destination.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
 
-// import destinationMap from "../assets/img/kayakweb.jpg";
+import destinationMap from "../assets/img/zoneLivraison.jpg";
+import "./content.css";
 
 function Destination() {
     return (
-        <Row className="my-5">
+        <Row className="my-3">
             <Col xl={11} lg={11} className="mt-5 mb-1 offset-xl-1 offset-lg-1">
                 <h3>Notre zone de livraison</h3>
             </Col>
-            <Col xl={3} lg={4} className="offset-xl-1 offset-lg-1">
+            <Col xl={2} lg={4} className="offset-xl-1 offset-lg-1">
                 <img
                     width="100%"
-                    src="https://www.touradour.com/torent/images/france-aquitaine-torent.png"
+                    src={destinationMap}
                     alt="zone de livraison transport Bayonne pays basque sud landes"
-                    className=""
+                    className="deliveryImg"
                 />
             </Col>
-            <Col xl={7} lg={6} className="text-justify align-self-start">
+            <Col xl={7} lg={6} className="text-justify align-self-start my-3">
                 Notre société, basée en périphérie de Bayonne, est en mesure de
                 livrer vos colis dans une zone qui s’étend de la frontière
                 espagnole, dans les Pyrénées Atlantiques jusque dans les Landes
-                voisines. La possibilité de livrer des colis volumineux ou sur
-                des distances plus longues sera étudiée au cas par cas afin de
-                vous satisfaire au mieux.
+                voisines.
+                <br />
+                La possibilité de livrer des colis volumineux ou sur des
+                distances plus longues sera étudiée au cas par cas afin de vous
+                satisfaire au mieux.
             </Col>
         </Row>
     );
